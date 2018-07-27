393b233cb7406e203691d9974b5984db8e5a07af
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-27 09:57:08 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number=10;
+        if(number>0)
+ {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+
+	}
+}
