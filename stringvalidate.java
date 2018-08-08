5784b43ba56dab0c5a840fe1b2433e712f3619af
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-08 11:56:14 +0530

    Create stringvalidate.java

---
+++ stringvalidate.java
@@ -0,0 +1,19 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String string = "12345.15";
+        boolean numeric = true;
+
+        try {
+            Double num = Double.parseDouble(string);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println(string + " is a number");
+        else
+            System.out.println(string + " is not a number");
+	}
+}
