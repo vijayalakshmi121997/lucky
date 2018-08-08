6a12b408e6145ea13b9c323d48468efd52d4635d
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-08 12:10:21 +0530

    Create mins to hours.java

---
+++ mins to hours.java
@@ -0,0 +1,9 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 double runtime = 185;
+ 
+    double hours = 185/60;
+ 
+    System.out.println("The duration of the movie is " + hours + " hours long.");
