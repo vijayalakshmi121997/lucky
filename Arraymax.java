52f500127d651f25ad3a44656aace6183c569f17
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-08 11:14:12 +0530

    Create Arraymax.java

---
+++ Arraymax.java
@@ -0,0 +1,24 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int n, max;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        max = a[0];
+        for(int i = 1; i < n; i++)
+        {
+            if(max < a[i])
+            {
+                max = a[i];
+            }
+        }
+        System.out.println(max);
+
+	}
+}
