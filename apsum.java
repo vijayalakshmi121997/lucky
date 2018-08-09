f64e8afb2f62e7c601f3aefa91274554ab3d0de8
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-09 10:36:09 +0530

    Create apsum.java

---
+++ apsum.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,a,d,val=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		a=sc.nextInt();
+		d=sc.nextInt();
+	            val=(n*(2*a+(n-1)*d))/2;
+                        System.out.println(val);
+	}
+}
