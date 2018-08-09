ccac499a9505d1a5a40eefb0b87677965b3b5bb8
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-09 12:34:55 +0530

    Create swap2.java

---
+++ swap2.java
@@ -0,0 +1,14 @@
+	 int x, y, temp;
+      System.out.println("Enter x and y");
+      Scanner in = new Scanner(System.in);
+ 
+      x = in.nextInt();
+      y = in.nextInt();
+ 
+      System.out.println("Before Swapping\nx = "+x+"\ny = "+y);
+ 
+      temp = x;
+      x = y;
+      y = temp;
+ 
+      System.out.println("After Swapping\nx = "+x+"\ny = "+y);
