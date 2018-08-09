caa9a0918c06e9bfe8631502700aa2c125429a2d
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-09 12:55:32 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int temp=0;
+ Scanner in= new Scanner(System.in);
+  int    x = in.nextInt();
+     int  y = in.nextInt();
+      temp = x;
+      x = y;
+      y = temp;
+ System.out.print(x+" "+y);
+	}// your code goes here
+	}
