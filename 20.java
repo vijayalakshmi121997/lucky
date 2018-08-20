8f3741563fdbaf53a503cebaaed77518351bf78a
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-20 12:04:50 +0530

    Create 20.java

---
+++ 20.java
@@ -0,0 +1,16 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int n, i;
+ 
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+ 
+    for(i=1; i<=5; ++i)
+    {
+  System.out.println(n*i);
+  System.out.println("\t");
+ }  
+	}
+}
