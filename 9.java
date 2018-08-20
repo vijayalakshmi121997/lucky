301e742d3d3fc820549110cf73ddd9840d2bf79b
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-20 11:59:37 +0530

    Create 9.java

---
+++ 9.java
@@ -0,0 +1,16 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int[] arr;
+      arr = new int[5];
+      arr[0] = 10;
+      arr[1] = 20;
+      arr[2] = 30;
+      arr[3] = 40;
+      arr[4] = 50;
+      for (int i = 0; i < arr.length; i++)
+         System.out.println("Element at index " + i + 
+                                      " : "+ arr[i]);    
+	}
+}
